9ebd3dead1253e75358a2cc144c66b27f1cc989a
Anthony Lopes <125279600+AnthonyVLopes@users.noreply.github.com>
2024-11-12 17:05:10 -0300

    projeto criado

---
+++ src/test/java/com/anthonylopes/List/project/ListProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anthonylopes.List.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
